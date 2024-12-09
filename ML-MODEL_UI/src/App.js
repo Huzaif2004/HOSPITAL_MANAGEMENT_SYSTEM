fe2181ae140a11e875ace789c58935dfaf00dce4
Huzaif2004 <124855224+Huzaif2004@users.noreply.github.com>
2024-12-09 21:29:44 +0530

    Add files via upload

---
+++ ML-MODEL_UI/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import DiabetesPredictor from './components/DiabetesPredictor';
+import HeartDiseaseForm from './components/HeartDiseasePredictor';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DiabetesPredictor/> */}
+      <HeartDiseaseForm/>
+      {/* <DiabetesPredictor/> */}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ML-MODEL_UI/src/components/DiabetesPredictor.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import './DiabetesPredictor.css'; // Import the new CSS file
+
+function DiabetesPredictor() {
+  const [formData, setFormData] = useState({
+    glucose: '',
+    insulin: '',
+    bmi: '',
+    age: ''
+  });
+
+  const [prediction, setPrediction] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // Handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+
+    const response = await fetch('http://127.0.0.1:5000/predict', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    });
+
+    const result = await response.json();
+    setPrediction(result.prediction);
+  };
+
+  return (
+    <div className="diabetes-predictor-login">
+      <h1>Diabetes Predictor</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="glucose"
+          placeholder="Glucose Level"
+          required
+          value={formData.glucose}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="insulin"
+          placeholder="Insulin"
+          required
+          value={formData.insulin}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="bmi"
+          placeholder="BMI"
+          required
+          value={formData.bmi}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="age"
+          placeholder="Age"
+          required
+          value={formData.age}
+          onChange={handleInputChange}
+        />
+        <button type="submit" className="btn btn-primary btn-block btn-large">
+          Predict
+        </button>
+      </form>
+
+      {isSubmitted && prediction && (
+        <div className="diabetes-predictor-result">
+          <p>{prediction}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DiabetesPredictor;
